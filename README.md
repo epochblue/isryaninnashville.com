fa74efe624f164fcbc49fd6dad65d9228d27cf6e
Bill Israel <bill.israel@gmail.com>
2014-06-28 10:31:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# isryaninnashville.com
+
+## About
+
+This single-serving website helps you determine if
+[Ryan Weaver](https://twitter.com/weaverryan) is currently
+in Nashville, or if he's traveling. You'd be amazed how
+difficult it is to remember this information.
+
+The app was built to run on [Heroku](http://heroku.com). This
+should explain the strangeness of using PostgreSQL to store
+Ryan's current status. If anyone knows a better way around the
+ephemeral nature of single-dyno Heroku apps, I'm all ears.
+
+
+## LICENSE
+
+Please consider this code to be in the public domain. Take, use,
+and alter it as you see fit.
+
---
+++ ryanweaver.py
@@ -0,0 +1,60 @@
+import os
+import postgres
+from functools import wraps
+
+from flask import Flask, request, render_template, jsonify
+
+
+DATABASE = os.environ.get('DATABASE_URL')
+
+
+app = Flask(__name__)
+token = os.environ.get('RYANWEAVER')
+db = postgres.Postgres(DATABASE)
+
+
+def _is_in_town():
+    return db.one("SELECT traveling FROM status")
+
+
+def _set_in_town(in_town):
+    db.run('UPDATE status SET traveling = %s', (in_town,))
+
+
+def auth_required(f):
+    """
+    Accepts either a X-Auth-Token header, or an auth_token parameter.
+    """
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        auth_token = request.headers.get('X-Auth-Token',
+                                         request.args.get('auth_token', None))
+        if auth_token != token:
+            return jsonify(error='Unauthorized'), 401
+
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', in_town=_is_in_town())
+
+
+@app.route('/traveling', methods=['POST', 'PUT'])
+@auth_required
+def traveling():
+    _set_in_town(False)
+    return jsonify(in_town=_is_in_town())
+
+
+@app.route('/in-town', methods=['POST', 'PUT'])
+@auth_required
+def not_traveling():
+    _set_in_town(True)
+    return jsonify(in_town=_is_in_town())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
